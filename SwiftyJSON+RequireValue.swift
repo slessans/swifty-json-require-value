5cea6ada4b55a359b1309fee2fcdd24cf80aeff8
Scott Lessans <scottl021@mac.com>
2016-07-27 10:16:01 -0700

    Update SwiftyJSON+RequireValue.swift

--- SwiftyJSON+RequireValue.swift
+++ SwiftyJSON+RequireValue.swift
@@ -9,15 +9,54 @@
 import Foundation
 import SwiftyJSON
 
+// TODO: better nested error messages (for instance, requireArrayOfValues doesnt give context on type error)
+
+enum JsonDeserializationError: ErrorType {
+    case PathError(json: JSON, path: [JSONSubscriptType], underlying: ErrorType)
+    case TypeError(json: JSON, expected: Any.Type, actual: Type)
+}
+
+extension JsonDeserializationError: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .PathError(_, let path, let underlying):
+            return "Invalid path \"\(path)\": \(underlying)"
+        case .TypeError(_, let expected, let actual):
+            return "Expected \(expected), got \(actual)"
+        }
+    }
+}
+
 extension JSON {
-    func requireValue<T>(atPath path: [JSONSubscriptType], transform: (JSON) -> T?) throws -> T {
-        let rep = self[path]
-        if let val = transform(rep) {
+    func requireType<T>(transform: (JSON) -> T?) throws -> T {
+        if let val = transform(self) {
             return val
         }
-        throw rep.error!
+        throw JsonDeserializationError.TypeError(json: self, expected: T.self, actual: self.type)
+    }
+    func requireArrayOfType<T>(transform: (JSON) -> T?) throws -> [T] {
+        return try self.requireType({ $0.array }).map({ try $0.requireType(transform) })
+    }
+    func requireExists(atPath path: [JSONSubscriptType]) throws -> JSON {
+        let rep = self[path]
+        if let e = rep.error {
+            throw JsonDeserializationError.PathError(json: self, path: path, underlying: e)
+        }
+        if !rep.exists() {
+            fatalError("should have been caught by error above?")
+        }
+        return rep
+    }
+    func requireValue<T>(atPath path: [JSONSubscriptType], transform: (JSON) -> T?) throws -> T {
+        return try self.requireExists(atPath: path).requireType(transform)
     }
     func requireValue<T>(atPath path: JSONSubscriptType, transform: (JSON) -> T?) throws -> T {
         return try requireValue(atPath: [path], transform: transform)
+    }
+    func requireArrayOfValues<T>(atPath path: [JSONSubscriptType], transform: (JSON) -> T?) throws -> [T] {
+        return try self.requireExists(atPath: path).requireType({ $0.array }).map({ try $0.requireType(transform) })
+    }
+    func requireArrayOfValues<T>(atPath path: JSONSubscriptType, transform: (JSON) -> T?) throws -> [T] {
+        return try requireArrayOfValues(atPath: [path], transform: transform)
     }
 }
