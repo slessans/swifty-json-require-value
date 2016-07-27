8799808d7006a23c9e26b6f95b027278f7a6e15a
Scott Lessans <scottl021@mac.com>
2016-07-27 00:12:00 -0700

    Create SwiftyJSON+RequireValue.swift

---
+++ SwiftyJSON+RequireValue.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftyJSON+RequireValue.swift
+//  Scott Lessans
+//
+//  Created by Scott Lessans on 7/27/16.
+//  Copyright © 2016 Scott Lessans. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+extension JSON {
+    func requireValue<T>(atPath path: [JSONSubscriptType], transform: (JSON) -> T?) throws -> T {
+        let rep = self[path]
+        if let val = transform(rep) {
+            return val
+        }
+        throw rep.error!
+    }
+}
+
+
+
+
